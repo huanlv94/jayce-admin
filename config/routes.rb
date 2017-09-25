aee2397cb524c0abf06ab5f7f792d907112fb67a
huanlv <huanlv@topica.edu.vn>
2017-09-25 14:32:37 +0700

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resourses :welcome do
+  resources :welcome do
     collection do
       get '', to: 'welcome#index'
       get 'index', to: 'welcome#index'
